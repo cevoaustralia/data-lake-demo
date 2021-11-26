cac3053756b735e695d04204efee4afacb616fb5
Jaehyeon Kim <dottami@gmail.com>
2021-11-27 06:59:23 +1100

    folder changed

--- connectors/note.sh
+++
@@ -1,9 +0,0 @@
-curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" \
-  http://localhost:8083/connectors/ -d @source-debezium.json
-
-curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" \
-  http://localhost:8083/connectors/ -d @sink-s3.json
-
-##
-curl http://localhost:8083/connectors/orders-source/status
-curl http://localhost:8083/connectors/orders-sink/status
