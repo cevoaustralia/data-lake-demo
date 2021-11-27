25efe0921c7d482940a4868f4212c81da4777ed1
Jaehyeon Kim <dottami@gmail.com>
2021-11-27 22:05:36 +1100

    add path name to files

--- connectors-local/setup.sh
+++ connectors-local/setup.sh
@@ -1,8 +1,8 @@
 curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" \
-  http://localhost:8083/connectors/ -d @source-debezium.json
+  http://localhost:8083/connectors/ -d @connectors-local/source-debezium.json
 
 curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" \
-  http://localhost:8083/connectors/ -d @sink-s3.json
+  http://localhost:8083/connectors/ -d @connectors-local/sink-s3.json
 
 ##
 curl http://localhost:8083/connectors/orders-source/status
