9e6c71067682817ffa44267190967c40bc83a26d
Jaehyeon Kim <dottami@gmail.com>
2021-12-07 22:00:49 +1100

    generate avro schema

--- connector/local/setup.sh
+++ connector/local/setup.sh
@@ -7,3 +7,17 @@ curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json"
 ##
 curl http://localhost:8083/connectors/orders-source/status
 curl http://localhost:8083/connectors/orders-sink/status
+
+#### TEST
+curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" \
+  http://localhost:8083/connectors/ -d @connector/local/source-debezium-avro.json
+
+curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" \
+  http://localhost:8083/connectors/ -d @connector/local/source-debezium-simple.json
+
+curl http://localhost:8083/connectors/orders-source-avro/status
+curl http://localhost:8083/connectors/orders-source-simple/status
+
+
+curl http://localhost:8081/subjects
+# ["avro.datalake.cdc_events-key","avro.datalake.cdc_events-value"]
